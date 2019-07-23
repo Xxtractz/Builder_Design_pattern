6f68d00e9fc01d4f8626f60058a10fb38ed397c0
Xxtractz <musambaloyi@gmail.com>
2019-07-23 17:48:17 +0200

    working on overide

--- src/main/java/co/builder/co/za/Main.java
+++ src/main/java/co/builder/co/za/Main.java
@@ -3,6 +3,10 @@ package co.builder.co.za;
 public class Main {
 
     public static void main(String[] args) {
-
+        User Musa = new User.UserBuilder("Musa", "Baloyi")
+                .age(21)
+                .address("")
+                .phoneNumber("")
+                .build();
     }
 }
