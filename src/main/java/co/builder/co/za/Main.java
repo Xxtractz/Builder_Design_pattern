b96c0c129c174131e5e6a051c9b55b4d72e9e218
Xxtractz <musambaloyi@gmail.com>
2019-07-23 17:43:02 +0200

    update

---
+++ src/main/java/co/builder/co/za/Main.java
@@ -0,0 +1,8 @@
+package co.builder.co.za;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/co/builder/co/za/User.java
@@ -0,0 +1,50 @@
+package co.builder.co.za;
+
+public class User
+{
+    private final String firstName;
+    private final String lastName;
+    private final String phoneNumber;
+    private final String address;
+    private final int age;
+
+    public User(UserBuilder builder){
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.age = builder.age;
+        this.phoneNumber = builder.phoneNumber;
+        this.address = builder.address;
+    }
+
+    public static class UserBuilder{
+        private final String firstName;
+        private final String lastName;
+        private String phoneNumber;
+        private String address;
+        private int age;
+
+        public UserBuilder(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public UserBuilder age(int age){
+            this.age = age;
+            return this;
+        }
+
+        public UserBuilder phoneNumber(String phoneNumber){
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public UserBuilder address(String address{
+            this.address = address;
+            return this;
+        }
+
+        public User build(){
+            return new User(this);
+        }
+    }
+}
